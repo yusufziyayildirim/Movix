abcd7243f6819025054232fe9060ae7a613fdacc
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-07-08 21:02:33 +0300

    Update AppDelegate.swift

--- Movix/Resources/AppDelegate.swift
+++ Movix/Resources/AppDelegate.swift
@@ -10,10 +10,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let container = DependencyContainer.shared
+        
+        //Register NetworkManager for API Calls
+        //container.register(type: NetworkManager.self, object: AlamofireNetworkManager.shared)
+        container.register(type: NetworkManager.self, object: URLSessionNetworkManager.shared)
+        
         return true
     }
 
