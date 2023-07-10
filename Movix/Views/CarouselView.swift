df7ce61934802e737a1018c8120a6418922fd6f0
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-07-10 19:20:23 +0300

    Create CarouselView

---
+++ Movix/Views/CarouselView.swift
@@ -0,0 +1,106 @@
+//
+//  CarouselView.swift
+//  Movix
+//
+//  Created by Yusuf Ziya YILDIRIM on 9.07.2023.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct CarouselView: View {
+    let movies: [Movie]
+    
+    init(movies: [Movie]) {
+        self.movies = movies
+    }
+
+    @State private var activeIndex = 0
+    @State private var dragOffset: CGFloat = 0
+    @State private var isAnimatedOffset = false
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    ZStack {
+                        ForEach(Array(movies.enumerated()), id: \.element.id) { index, movie in
+                            let url = URL(string: ApiRoutes.imageURL(posterPath: movie.posterPath ?? ""))
+                            WebImage(url: url)
+                                .resizable()
+                                .clipShape(RoundedRectangle(cornerRadius: 30))
+                                .offset(x: getOffset(index: index, geometry: geometry))
+                                .scaleEffect(getScale(index: index))
+                                .opacity(getOpacity(index: index))
+                                .shadow(color: Color(.black).opacity(0.8), radius: 5, x: 0, y: 0)
+                        }
+                    }
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation {
+                                    isAnimatedOffset = true
+                                    dragOffset = value.translation.width
+                                }
+                            }
+                            .onEnded { value in
+                                withAnimation {
+                                    isAnimatedOffset = true
+                                    let dragThreshold: CGFloat = geometry.size.width / 5
+                                    if value.translation.width > dragThreshold {
+                                        activeIndex = max(activeIndex - 1, 0)
+                                    } else if value.translation.width < -dragThreshold {
+                                        activeIndex = min(activeIndex + 1, movies.count - 1)
+                                    }
+                                    dragOffset = 0
+                                }
+                            }
+                    )
+                    
+                    HStack(spacing: 7) {
+                        ForEach(0..<movies.count, id: \.self) { index in
+                            Circle()
+                                .fill(activeIndex == index ? Color.primary : Color.gray)
+                                .frame(width: 7, height: 7)
+                        }
+                    }
+                    .padding(.top, 32)
+                }
+                .frame(width: geometry.size.width)
+            }
+        }
+        .padding(.horizontal, 20)
+    }
+    
+    private func getOffset(index: Int, geometry: GeometryProxy) -> CGFloat {
+        let itemWidth = geometry.size.width
+        let offset = CGFloat(index - activeIndex) * itemWidth + dragOffset
+        return offset
+    }
+    
+    private func getScale(index: Int) -> CGFloat {
+        let scale: CGFloat = activeIndex == index ? 1 : 0.86
+        return scale
+    }
+    
+    private func getOpacity(index: Int) -> Double {
+        let opacity: Double = activeIndex == index ? 1 : 0
+        return opacity
+    }
+}
+
+
+struct CarouselView_Previews: PreviewProvider {
+   
+    static var previews: some View {
+        let movies: [Movie] = [
+            Movie(id: 2, overview: "", posterPath: "9n2tJBplPbgR2ca05hS5CKXwP2c.jpg", releaseDate: "", title: "", voteAverage: 7.5 , runtime: 44, genres: [Genre(id: 1, name: "")]),
+            Movie(id: 3, overview: "", posterPath: "9n2tJBplPbgR2ca05hS5CKXwP2c.jpg", releaseDate: "", title: "", voteAverage: 7.5 , runtime: 44, genres: [Genre(id: 1, name: "")])
+        ]
+        
+        CarouselView(movies: movies)
+    }
+}
+
