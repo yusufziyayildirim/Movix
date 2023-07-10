46bb04858af28e35569d4686172798cade9cc7c0
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-07-10 19:18:38 +0300

    Create Movie model

---
+++ Movix/Models/Movie.swift
@@ -0,0 +1,23 @@
+//
+//  Movie.swift
+//  Movix
+//
+//  Created by Yusuf Ziya YILDIRIM on 9.07.2023.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    let id: Int?
+    let overview, posterPath, releaseDate, title: String?
+    let voteAverage: Double?
+    let runtime: Int?
+    let genres: [Genre]?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, overview, runtime, genres
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+    }
+}
