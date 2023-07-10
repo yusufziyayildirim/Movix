81d1dca2670a1616188a68cc978da121a468303e
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-07-10 22:47:58 +0300

    Design DiscoverVC and Implement UIHostingController for DiscoveView

--- Movix/Controllers/TabBarControllers/DiscoverVC.swift
+++ Movix/Controllers/TabBarControllers/DiscoverVC.swift
@@ -6,24 +6,49 @@
 //
 
 import UIKit
+import SwiftUI
 
 class DiscoverVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let discoverView = UIHostingController(rootView: DiscoverView())
+        discoverView.view.frame = view.frame
+        
+        addChild(discoverView)
+        view.addSubview(discoverView.view)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        if let tabBarController = self.tabBarController {
+            let tabIndex = 1
+            
+            // TabBarItem'ı alın
+            if let tabBarItem = tabBarController.tabBar.items?[tabIndex] {
+                // Yeni görüntüyü oluşturun
+                let newImage = UIImage(systemName: "flashlight.on.fill")
+                
+                // Yeni görüntüyü TabBarItem'a atayın
+                tabBarItem.image = newImage
+            }
+        }
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        if let tabBarController = self.tabBarController {
+            let tabIndex = 1
+            
+            // TabBarItem'ı alın
+            if let tabBarItem = tabBarController.tabBar.items?[tabIndex] {
+                // Yeni görüntüyü oluşturun
+                let newImage = UIImage(systemName: "flashlight.off.fill")
+                
+                // Yeni görüntüyü TabBarItem'a atayın
+                tabBarItem.image = newImage
+            }
+        }
+    }
 
 }
