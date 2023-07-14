2c4ebc76ea0904a96b83810f8385c9eea6288455
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-07-14 17:47:05 +0300

    Create MovieTrailerKey model

---
+++ Movix/Models/MovieTrailerKey.swift
@@ -0,0 +1,13 @@
+//
+//  MovieTrailerKey.swift
+//  Movix
+//
+//  Created by Yusuf Ziya YILDIRIM on 13.07.2023.
+//
+
+import Foundation
+
+struct MovieTrailerKey: Codable {
+    let key: String?
+    let type: String?
+}
