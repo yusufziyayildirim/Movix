d8cdc9f5b806ac2a1434cb637cf8235a000b8633
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-07-15 23:34:58 +0300

    Create SavedMoviesViewModel.swift

---
+++ Movix/ViewModels/OtherViewModels/SavedMoviesViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  SavedMoviesViewModel.swift
+//  Movix
+//
+//  Created by Yusuf Ziya YILDIRIM on 15.07.2023.
+//
+
+import Foundation
+
+class SavedMoviesViewModel{
+    
+    weak var delegate: SavedMoviesViewModelDelegate?
+    
+    var movies: [Movie] = []
+    
+    func getSavedMovies(status: MovieStatus){
+        do{
+            let result = try CoreDataManager.shared.fetchMovies(withStatus: status)
+            self.movies = result
+            delegate?.reloadTableView()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func removeMovie(with row: Int, status: MovieStatus) {
+        do {
+            try CoreDataManager.shared.removeMovie(movie: movies[row], status: status)
+            movies.remove(at: row)
+            print(movies.count)
+            delegate?.reloadTableView()
+        } catch {
+            print("Failed to fetch movies with status search history: \(error)")
+        }
+    }
+}
