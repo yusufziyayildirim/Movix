d966d25dca37c5b6ba37de333a042164978ec277
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-07-15 23:35:52 +0300

    Design SavedMoviesListVC and integrate SavedMoviesViewModel

--- Movix/Controllers/OtherControllers/SavedMoviesListVC.swift
+++ Movix/Controllers/OtherControllers/SavedMoviesListVC.swift
@@ -7,23 +7,146 @@
 
 import UIKit
 
-class SavedMoviesListVC: UIViewController {
+protocol SavedMoviesViewModelDelegate: AnyObject{
+    func reloadTableView()
+}
 
+class SavedMoviesListVC: UIViewController {
+    
+    @IBOutlet weak var moviesTableView: UITableView!
+    
+    let viewModel = SavedMoviesViewModel()
+    let cell = MovieTableViewCell()
+    
+    var status: MovieStatus = .watchlist
+    var pageTitle = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.title = pageTitle
+        
+        configureMovieTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func configureMovieTableView() {
+        moviesTableView.delegate = self
+        moviesTableView.dataSource = self
+        moviesTableView.register(cell.nib, forCellReuseIdentifier: cell.id)
+        
+        viewModel.getSavedMovies(status: status)
     }
-    */
+    
+    func navigateToDetailScreen(with id: Int) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let destinationVC = storyboard.instantiateViewController(withIdentifier: "MovieDetailVC") as! MovieDetailVC
+        destinationVC.movieId = id
+        navigationController?.pushViewController(destinationVC, animated: true)
+    }
+    
+    func createEmptySearchView() -> UIView {
+        let messageLabel = UILabel()
+        messageLabel.text = "No saved movies available"
+        messageLabel.textColor = UIColor.gray
+        messageLabel.textAlignment = .center
+        messageLabel.numberOfLines = 0
+        messageLabel.font = UIFont.systemFont(ofSize: 17)
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let emptySearchView = UIView()
+        emptySearchView.addSubview(messageLabel)
+        
+        messageLabel.pinToEdgesOf(view: emptySearchView)
+        return emptySearchView
+    }
+    
+}
 
+extension SavedMoviesListVC: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if viewModel.movies.isEmpty {
+            tableView.backgroundView = createEmptySearchView()
+            tableView.separatorStyle = .none
+            return 0
+        } else {
+            return viewModel.movies.count
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let movieTableViewCell = tableView.dequeueReusableCell(withIdentifier: cell.id, for: indexPath) as? MovieTableViewCell else {
+            return UITableViewCell()
+        }
+        movieTableViewCell.setData(with: viewModel.movies[indexPath.row])
+        
+        return movieTableViewCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        navigateToDetailScreen(with: viewModel.movies[indexPath.row].id ?? 0)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            viewModel.removeMovie(with: indexPath.row, status: status)
+            moviesTableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        let selectedMovie = viewModel.movies[indexPath.row]
+        
+        let config = UIContextMenuConfiguration(
+            identifier: nil,
+            previewProvider: { () -> UIViewController? in
+                let imagePreviewVC = ImagePreviewVC(posterPath: selectedMovie.posterPath ?? "")
+                return imagePreviewVC
+            }) { _ in
+                
+                let favoriteButtonViewModel = MovieActionButtonViewModel(movie: selectedMovie, status: .favoriteMovies, imageName: "star")
+                let watchlistButtonViewModel = MovieActionButtonViewModel(movie: selectedMovie, status: .watchlist, imageName: "bookmark")
+                let historyButtonViewModel = MovieActionButtonViewModel(movie: selectedMovie, status: .watchHistory, imageName: "checkmark.seal")
+                
+                let favoriteAction = UIAction(title: favoriteButtonViewModel.buttonTitle, image: favoriteButtonViewModel.buttonImage) { _ in
+                    favoriteButtonViewModel.performAction()
+                    if self.status == .favoriteMovies {
+                        self.viewModel.movies.remove(at: indexPath.row)
+                        self.moviesTableView.deleteRows(at: [indexPath], with: .fade)
+                    }
+                }
+                
+                let watchlistAction = UIAction(title: watchlistButtonViewModel.buttonTitle, image: watchlistButtonViewModel.buttonImage) { _ in
+                    watchlistButtonViewModel.performAction()
+                    if self.status == .watchlist {
+                        self.viewModel.movies.remove(at: indexPath.row)
+                        self.moviesTableView.deleteRows(at: [indexPath], with: .fade)
+                    }
+                }
+                
+                let historyAction = UIAction(title: historyButtonViewModel.buttonTitle, image: historyButtonViewModel.buttonImage) { _ in
+                    historyButtonViewModel.performAction()
+                    if self.status == .watchHistory {
+                        self.viewModel.movies.remove(at: indexPath.row)
+                        self.moviesTableView.deleteRows(at: [indexPath], with: .fade)
+                    }
+                }
+                
+                return UIMenu(title: "", image: nil, identifier: nil, options: .displayInline, children: [favoriteAction, watchlistAction, historyAction])
+                
+            }
+        return config
+        
+    }
+}
+
+extension SavedMoviesListVC: SavedMoviesViewModelDelegate {
+    func reloadTableView() {
+        self.moviesTableView.reloadOnMainThread()
+    }
 }
